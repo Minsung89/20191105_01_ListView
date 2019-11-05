08e3f8688f37085253dd4d72a7ef6f4c0e978c7a
Minsung89 <minsung0217@gmail.com>
2019-11-05 21:46:52 +0900

    데이터 밴수들을 리스트에 담아둠

--- app/src/main/java/com/tjoeum/a20191105_01_listview/ListViewPracticeActivity.kt
+++ app/src/main/java/com/tjoeum/a20191105_01_listview/ListViewPracticeActivity.kt
@@ -2,11 +2,29 @@ package com.tjoeum.a20191105_01_listview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tjoeum.a20191105_01_listview.datas.PostData
 
 class ListViewPracticeActivity : AppCompatActivity() {
+
+    var postList = ArrayList<PostData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_view_practice)
+
+        addPosts()
+    }
+
+    fun  addPosts(){
+        postList.add(PostData("1 글입니다.","내용을 1번째로 적습니다."))
+        postList.add(PostData("2 글입니다.","내용을 2번째로 적습니다."))
+        postList.add(PostData("3 글입니다.","내용을 3번째로 적습니다."))
+        postList.add(PostData("4 글입니다.","내용을 4번째로 적습니다."))
+        postList.add(PostData("5 글입니다.","내용을 5번째로 적습니다."))
+        postList.add(PostData("6 글입니다.","내용을 6번째로 적습니다."))
+        postList.add(PostData("7 글입니다.","내용을 7번째로 적습니다."))
+        postList.add(PostData("8 글입니다.","내용을 8번째로 적습니다."))
+        postList.add(PostData("9 글입니다.","내용을 9번째로 적습니다."))
+
     }
 }
