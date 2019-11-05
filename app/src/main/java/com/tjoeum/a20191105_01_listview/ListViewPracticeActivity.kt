553de540d0768f908f8c7d0e40bd991f30a7f2fa
Minsung89 <minsung0217@gmail.com>
2019-11-05 21:39:19 +0900

    새 액티비티 생성, 리스트뷰 생성

---
+++ app/src/main/java/com/tjoeum/a20191105_01_listview/ListViewPracticeActivity.kt
@@ -0,0 +1,12 @@
+package com.tjoeum.a20191105_01_listview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListViewPracticeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_view_practice)
+    }
+}
--- app/src/main/java/com/tjoeum/a20191105_01_listview/adapters/RoomDataAdapter.kt
+++ app/src/main/java/com/tjoeum/a20191105_01_listview/adapters/RoomDataAdapter.kt
@@ -11,16 +11,9 @@ import com.tjoeum.a20191105_01_listview.datas.RoomData
 
 class RoomDataAdapter(context:Context, resource:Int, list : ArrayList<RoomData>) : ArrayAdapter<RoomData>(context, resource, list) {
 
-    var mContext:Context? = null
-    var mList:ArrayList<RoomData>? = null
-    var inf:LayoutInflater? = null
-
-    init {
-        mContext = context
-        mList = list
-        inf = LayoutInflater.from(mContext)
-    }
-
+    var mContext:Context? = context
+    var mList:ArrayList<RoomData>? = list
+    var inf:LayoutInflater? =  LayoutInflater.from(mContext)
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         var row = convertView
