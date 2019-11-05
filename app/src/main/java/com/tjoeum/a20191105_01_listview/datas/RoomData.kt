b82cefd5ed646deb64b1ad88913b4db3404f7d0a
Minsung89 <minsung0217@gmail.com>
2019-11-05 20:31:24 +0900

    방 데이터 클래스 추가  - 메인 액티비티 : 데이터 추가를 별개 메소드로 작성  - sum메소드로 코틀린만의 특이한 문법 확인

---
+++ app/src/main/java/com/tjoeum/a20191105_01_listview/datas/RoomData.kt
@@ -0,0 +1,10 @@
+package com.tjoeum.a20191105_01_listview.datas
+
+class RoomData {
+
+    var price : Int = 0
+
+    var address : String = ""
+
+    var floor : Int = 1; //음수 : 지하, -1층 : 지하1층 0층 : 반지하
+}
